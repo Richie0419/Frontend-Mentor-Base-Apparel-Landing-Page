ef583296c2bbef32b3ba4929d3565f86fae12842
Richie0419 <richmond.amoah1234@gmail.com>
2023-11-05 11:21:26 +0000

    Third Commit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const buttonBtn = document.querySelector('.button');
 const errorImg = document.querySelector('.error-img');
 const errorMsg = document.querySelector('.error-msg');
 const emailInput = document.getElementById('email');
+const formInput = document.querySelector('.input');
 
 buttonBtn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -11,18 +12,22 @@ buttonBtn.addEventListener('click', (e) => {
     if (emailValue === '') {
         errorImg.style.display = 'block';
         errorMsg.style.display = 'block';
+        formInput.style.border = '1px solid red';
 
         setTimeout(() => {
             errorImg.style.display = 'none';
             errorMsg.style.display = 'none';
+            formInput.style.border = '1px solid rgba(100, 90, 90, 0.5) ';
         }, 5000);
 
     } else if (!emailRegex.test(emailValue)) {
         errorMsg.textContent = 'Please provide a valid email';
         errorMsg.style.display = 'block';
+        formInput.style.border = '1px solid red';
 
         setTimeout(() => {
             errorMsg.style.display = 'none';
+            formInput.style.border = '1px solid rgba(100, 90, 90, 0.5)';
         }, 5000);
     } else {
         errorImg.style.display = 'none';
